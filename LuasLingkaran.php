c2cff820c1f02ee965e729df887cec04aed09baf
ragasatriaputrasofian <sragasatria@gmail.com>
2025-11-11 01:29:45 +0700

    first commit

--- LuasLingkaran.php
+++ LuasLingkaran.php
@@ -1,9 +1,22 @@
 <?php
 
+<<<<<<< HEAD
 class LuasLingkaran {
 
     public const phi = 3.14;
     public int $jari;
+=======
+namespace App\Math;
+
+class LuasLingkaran {
+
+    public const phi = 3.14;
+    protected int $jari;
+    
+    public function __construct($isiJari = 1) {
+        $this->jari = $isiJari; //10
+    }
+>>>>>>> 3cb0a3e (first commit)
 
     public function tampil($nama = 'ban') {
         $rumus = LuasLingkaran::phi * ($this->jari * $this->jari);
@@ -14,6 +27,7 @@ class LuasLingkaran {
         echo "<br/>";
         echo "ini dari static";
     }
+<<<<<<< HEAD
 }
 
 $lingkaran = new LuasLingkaran();
@@ -21,3 +35,11 @@ $lingkaran->jari = 12;
 $lingkaran->tampil('roda'); //panggil method tampil
 
 LuasLingkaran::testing(); //panggil static method
+=======
+
+    public function __destruct() {
+        echo "<br/>";
+        echo "udah ah cape";
+    }
+}
+>>>>>>> 3cb0a3e (first commit)
